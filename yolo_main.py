b9fe29dfdf0ff7244cb0710a77b2c3be72102841
Kevin Griffin <kevin.s.griffin@gmail.com>
2024-12-01 18:35:36 -0600

    Added command line args and main function

--- yolo_main.py
+++ yolo_main.py
@@ -4,77 +4,90 @@ import matplotlib.pyplot as plt
 from utils import *
 from darknet import Darknet
 
-# Setting up the Neural Network
-# =============================
+import argparse
 
-# Set the location and name of the cfg file
-cfg_file = './cfg/yolov3.cfg'
+def main(cfg_file, weight_file, namesfile, image_path):
+    # Setting up the Neural Network
+    # =============================
 
-# Set the location and name of the pre-trained weights file
-weight_file = './weights/yolov3.weights'
+    # Set the location and name of the cfg file
+    # cfg_file = './cfg/yolov3.cfg'
 
-# Set the location and name of the COCO (common objects in context) object classes file
-namesfile = 'data/coco.names'
+    # # Set the location and name of the pre-trained weights file
+    # weight_file = './weights/yolov3.weights'
 
-# Load the network architecture
-yolo_darknet = Darknet(cfg_file)
+    # # Set the location and name of the COCO (common objects in context) object classes file
+    # namesfile = 'data/coco.names'
 
-# Load the pre-trained weights
-yolo_darknet.load_weights(weight_file)
+    # Load the network architecture
+    yolo_darknet = Darknet(cfg_file)
 
-# Load the COCO object classes
-class_names = load_class_names(namesfile)
+    # Load the pre-trained weights
+    yolo_darknet.load_weights(weight_file)
 
-# Print the neural network architecture
-yolo_darknet.print_network()
+    # Load the COCO object classes
+    class_names = load_class_names(namesfile)
 
-# Load and pre-process an image
-# =============================
+    # Print the neural network architecture
+    yolo_darknet.print_network()
 
-# Set the default figure size
-plt.rcParams['figure.figsize'] = [24.0, 14.0]
+    # Load and pre-process an image
+    # =============================
 
-# Load the image
-img = cv2.imread('images/dog.jpg')
+    # Set the default figure size
+    plt.rcParams['figure.figsize'] = [24.0, 14.0]
 
-# Convert the image to RGB
-original_image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    # Load the image
+    img = cv2.imread(image_path)
 
-# We rescale the image to 416x416 to be compatible with the first layer of the darknet model
-resized_image = cv2.resize(original_image, (yolo_darknet.width, yolo_darknet.height))
+    # Convert the image to RGB
+    original_image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-# Display the image
-# plt.subplot(121) # 121 means 1x2 grid, first subplot
-# plt.title('Original Image')
-# plt.imshow(original_image)
-# # plt.axis('off')
-# plt.subplot(122) # 122 means 1x2 grid, second subplot
-# plt.title('Resized Image')
-# plt.imshow(resized_image)
-# plt.show()
+    # We rescale the image to 416x416 to be compatible with the first layer of the darknet model
+    resized_image = cv2.resize(original_image, (yolo_darknet.width, yolo_darknet.height))
 
-# Fine-Tuning YOLOv3
-# ========================
+    # Display the image
+    # plt.subplot(121) # 121 means 1x2 grid, first subplot
+    # plt.title('Original Image')
+    # plt.imshow(original_image)
+    # # plt.axis('off')
+    # plt.subplot(122) # 122 means 1x2 grid, second subplot
+    # plt.title('Resized Image')
+    # plt.imshow(resized_image)
+    # plt.show()
 
-# Set the NMS threshold
-# YOLO uses Non-Maximal Suppression (NMS) to only keep the best bounding box. The first step
-# in NMS is to remove all the predicted bounding boxes that have a detection probability that
-# is less than a given NMS threshold. In the code below, we set this NMS threshold to 0.6.
-# This means that all predicted bounding boxes that have a detection probability less than
-# 0.6 will be removed
-nms_thresh = 0.6
+    # Fine-Tuning YOLOv3
+    # ========================
 
-# Set the IOU threshold
-# The Intersection over Union (IOU) threshold is used to determine how much overlap is
-# required for two bounding boxes to be considered the same object. In the code below, we
-# set the IOU threshold to 0.4
-iou_thresh = 0.4
+    # Set the NMS threshold
+    # YOLO uses Non-Maximal Suppression (NMS) to only keep the best bounding box. The first step
+    # in NMS is to remove all the predicted bounding boxes that have a detection probability that
+    # is less than a given NMS threshold. In the code below, we set this NMS threshold to 0.6.
+    # This means that all predicted bounding boxes that have a detection probability less than
+    # 0.6 will be removed
+    nms_thresh = 0.6
 
-# Object Detection
-# ================
-boxes = detect_objects(yolo_darknet, resized_image, iou_thresh, nms_thresh)
+    # Set the IOU threshold
+    # The Intersection over Union (IOU) threshold is used to determine how much overlap is
+    # required for two bounding boxes to be considered the same object. In the code below, we
+    # set the IOU threshold to 0.4
+    iou_thresh = 0.4
 
-print_objects(boxes, class_names)
+    # Object Detection
+    # ================
+    boxes = detect_objects(yolo_darknet, resized_image, iou_thresh, nms_thresh)
 
-# Display the image with bounding boxes
-plot_boxes(original_image, boxes, class_names, plot_labels=True)
+    print_objects(boxes, class_names)
+
+    # Display the image with bounding boxes
+    plot_boxes(original_image, boxes, class_names, plot_labels=True)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="YOLO Object Detection")
+    parser.add_argument("--cfg", type=str, default="cfg/yolov3.cfg", help="path to configuration file")
+    parser.add_argument("--weights", type=str, default="weights/yolov3.weights", help="path to the pre-trained weights file")
+    parser.add_argument("--names", type=str, default="data/coco.names", help="path to class names file")
+    parser.add_argument("--image", type=str, default="images/test_real.png", help="image path")
+    args = parser.parse_args()
+
+    main(args.cfg, args.weights, args.names, args.image)
